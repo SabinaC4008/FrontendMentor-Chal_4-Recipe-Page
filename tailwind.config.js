a11f66f5c24bcc397bb912463f79f2ad090fd3a5
sabina4008 <sabinachauhan4008@gmail.com>
2024-11-22 11:52:51 -0800

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./recipe-page-main/**/*.{html,js}"],
+  theme: {
+    extend: {
+        fontFamily:{
+        Youngserif: ['Youngserif'],
+        Outfit: ['Outfit'],
+      },
+      colors:{
+        'custom-stone-light': '#f3e6d8', 
+        'custom-stone-light-deep': '#e4ded8', 
+        'custom-stone-deep': '#5f574e', 
+        'custom-stone-deep-dark': '#302d2c', 
+
+        'custom-brown': '#854632',
+
+        'deep-rose': '#7b284f',
+        'light-rose': '#fff5fa', 
+      },
+      width: {
+        '89': '89%',
+        '99': '365px',
+      },
+      height: {
+        'custom-height': '1080px',
+      },
+    },
+  },
+  plugins: [],
+}
+
